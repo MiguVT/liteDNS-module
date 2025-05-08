4074117594e5acba53c21dcc167a2707a23dfcf5
Yara <miguyt1@gmail.com>
2025-05-08 21:10:30 +0200

    fix: update FINAL_DIR in customize.sh to use harcoded id

--- liteDNS/customize.sh
+++ liteDNS/customize.sh
@@ -15,7 +15,7 @@ CONF_DELETED=1
 
 
 MODID=$(grep 'id=' "$MODPATH/module.prop" | cut -d= -f2)
-FINAL_DIR="/data/adb/modules/$MODID"
+FINAL_DIR="/data/adb/modules/liteDNS"
 FINAL_CONFIG="$FINAL_DIR/config.json"
 
 ui_print "Final config path: $FINAL_CONFIG"
